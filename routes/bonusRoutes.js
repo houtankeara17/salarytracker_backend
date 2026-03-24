a619c99416fe790deb3431e934fada12b8a397e1
houtankeara17 <houtankeara17@gmail.com>
2026-03-25 00:26:22 +0700

    update file name from bonusRouter to bonusRoutes

--- routes/bonusRoutes.js
+++ routes/bonusRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../controllers/bonusController");
+const ctrl = require("../controllers/BonusController");
 const { protect } = require("../middleware/auth");
 
 router.get("/", protect, ctrl.getAllBonuses);
